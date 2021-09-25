8c95528bd728cda9afed57bf77b4eefee3c70383
brian-le <lblt1995@gmail.com>
2021-09-25 12:57:44 -0500

    Fail test

--- __test__/unit.test.js
+++ __test__/unit.test.js
@@ -2,5 +2,5 @@ const greet = require("../src/controller").greet;
 
 test("Sample test", () => {
   const name = "Test Doe";
-  expect(greet(name)).toBe(`Greeting ${name}!`);
+  expect(greet(name)).toBe(`Greeting ${name}!.`);
 });

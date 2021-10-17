d782346470873a2a45265f1c4d192b22b56bfd1a
brian-le <lblt1995@gmail.com>
2021-10-17 18:08:12 -0500

    Update scripts

--- scripts/install_app_dependencies.sh
+++ scripts/install_app_dependencies.sh
@@ -2,6 +2,9 @@
 exec 3>&1 4>&2
 trap 'exec 2>&4 1>&3' 0 1 2 3
 exec 1>/home/ec2-user/out.log 2>&1
-source /home/ec2-user/.bashrc
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
 cd /home/ec2-user/github-actions-demo
 npm install
--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-source /home/ec2-user/.bashrc
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
 cd /home/ec2-user/github-actions-demo
 pm2 start src/serverSide/server.js

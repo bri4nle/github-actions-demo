56d69818c2f88a05482d70f1709423b86e8b4fea
brian-le <lblt1995@gmail.com>
2021-10-17 13:19:17 -0500

    Update scripts

--- scripts/install_app_dependencies.sh
+++ scripts/install_app_dependencies.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd /home/ec2-user/github-actions-demo
-npm i
+sudo npm install
--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cd /home/ec2-user/github-actions-demo/src/serverSide
-pm2 start server.js
+cd /home/ec2-user/github-actions-demo
+pm2 start src/serverSide/server.js

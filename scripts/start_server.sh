fee2b716f3f43cb0cd1958393b040e62236d2f9a
brian-le <lblt1995@gmail.com>
2021-10-17 18:10:10 -0500

    Update scripts

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" 
 
 cd /home/ec2-user/github-actions-demo
 pm2 start src/serverSide/server.js

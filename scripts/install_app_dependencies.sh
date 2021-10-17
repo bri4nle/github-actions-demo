c89aa485ea502baa2352f13f3ff6338a83974c20
brian-le <lblt1995@gmail.com>
2021-10-17 17:35:43 -0500

    Update scripts

--- scripts/install_app_dependencies.sh
+++ scripts/install_app_dependencies.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 exec 3>&1 4>&2
 trap 'exec 2>&4 1>&3' 0 1 2 3
-exec 1>/home/ubuntu/out.log 2>&1
+exec 1>/home/ec2-user/out.log 2>&1
 source /home/ec2-user/.bash_profile
 cd /home/ec2-user/github-actions-demo
 npm install

a819cdd52a47286a4440e5bd6e5f728cf8ecb276
Brian Le <38984844+bri4nle@users.noreply.github.com>
2021-10-18 09:13:12 -0500

    Update install_app_dependencies.sh

--- scripts/install_app_dependencies.sh
+++ scripts/install_app_dependencies.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
-exec 3>&1 4>&2
-trap 'exec 2>&4 1>&3' 0 1 2 3
-exec 1>/home/ec2-user/out.log 2>&1
+# exec 3>&1 4>&2
+# trap 'exec 2>&4 1>&3' 0 1 2 3
+# exec 1>/home/ec2-user/out.log 2>&1
 # source /home/ec2-user/.bashrc
+echo "starting script"
+pwd
+whoami
+ls
 
 cd /home/ec2-user/github-actions-demo
 npm install

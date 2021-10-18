f93bc5ec3290e7cdd83de5c386861dd7a845d928
Brian Le <38984844+bri4nle@users.noreply.github.com>
2021-10-18 09:52:26 -0500

    Update install_app_dependencies.sh

--- scripts/install_app_dependencies.sh
+++ scripts/install_app_dependencies.sh
@@ -3,8 +3,8 @@
 # trap 'exec 2>&4 1>&3' 0 1 2 3
 # exec 1>/home/ec2-user/out.log 2>&1
 echo "activating .bashrc file"
-export NVM_DIR="/home/ec2-user/.nvm" [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
-echo "starting script"
+export NVM_DIR="/home/ec2-user/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvmecho "starting script"
 echo $NVM_DIR
 pwd
 whoami

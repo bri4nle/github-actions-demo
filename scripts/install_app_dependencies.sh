14532cbbb85e4395cbf5afe82f1a4709e15b60bf
brian-le <lblt1995@gmail.com>
2021-10-20 13:31:46 -0500

    Fix path and export env

--- scripts/install_app_dependencies.sh
+++ scripts/install_app_dependencies.sh
@@ -3,7 +3,7 @@
 # trap 'exec 2>&4 1>&3' 0 1 2 3
 # exec 1>/home/ec2-user/out.log 2>&1
 echo "activating .bashrc file"
-export NODE_ENV="production"
+su - ec2-user -c "NODE_ENV=\"production\""
 export NVM_DIR="/home/ec2-user/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvmecho "starting script"
 echo $NVM_DIR

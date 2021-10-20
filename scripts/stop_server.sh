c80e515adf8bf7028c7d54e42810da65f9d00d6a
brian-le <lblt1995@gmail.com>
2021-10-20 12:54:44 -0500

    Fix path

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -2,5 +2,5 @@ export NVM_DIR="/home/ec2-user/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 echo "starting script"
 
-cd /home/ec2-user/github-actions-demo/src/serverSide
-pm2 stop server.js
+cd /home/ec2-user/github-actions-demo
+pm2 stop src/serverSide/server.js

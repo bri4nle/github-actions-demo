be24edbaf2121c17203bdedd89516d576502084c
brian-le <lblt1995@gmail.com>
2021-10-25 19:14:55 -0500

    Update

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -3,6 +3,6 @@ export NVM_DIR="/home/ec2-user/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 
 cd /home/ec2-user/github-actions-demo
-pm2 start src/serverSide/index.js
+pm2 start src/serverSide/index.js -f
 
 
